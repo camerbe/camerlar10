4ce6474ff8f16e4a80d74661f94d9597f46039ea
tristan <tristanvellozzi@gmail.com>
2023-11-18 01:18:43 +0100

    Les migrations

---
+++ database/migrations/2023_11_17_225835_create_publicites_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('publicites', function (Blueprint $table) {
+            $table->id();
+            $table->date('datefin');
+            $table->text('publicite');
+            $table->foreign('typepub_id')->references('id')->on('typepubs');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('publicites');
+    }
+};
---
+++ database/migrations/2023_11_17_235611_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->tinyText('titre');
+            $table->mediumText('chapeau');
+            $table->longText('article');
+            $table->string('image');
+            $table->string('keyword')->nullable();
+            $table->string('imagewidth')->nullable();
+            $table->string('imageheigth')->nullable();
+            $table->string('slug',300)->unique();
+            $table->dateTime('dateparution')->index();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('rubrique_id')->references('id')->on('rubriques');
+            //$table->foreign('pays_id')->references('code')->on('pays');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
