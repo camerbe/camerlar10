f63b6d12812291d1fd0846e4ce69f2cf51a92444
tristan <tristanvellozzi@gmail.com>
2023-11-21 23:36:54 +0100

    Seeder pour la table roles

--- app/Repositories/FrontEndRepository.php
+++ app/Repositories/FrontEndRepository.php
@@ -2,7 +2,6 @@
 
 namespace App\Repositories;
 
-use App\Helpers\ToolBox;
 use App\Models\Article;
 use App\Models\Pays;
 use App\Repositories\BaseRepository;
@@ -11,47 +10,72 @@ use Illuminate\Support\Str;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
-use Html2Text\Html2Text;
 
 
-abstract class FrontEndRepository 
+
+class FrontEndRepository
 {
     protected $perpageweb=20;
     protected $perpagemobile=10;
 
-    public static function getCMRNews($cam) {
-
+    public function getCMRNews($cam) {
         if(Cache::has($cam)){
             return Cache::get($cam)->take(100);
         }
-        Cache::remember($cam, now()->addHour(1),function(){
+        Cache::remember($cam, now()->addHour(1),function() use ($cam) {
             return Article::where('articles.pays_code',$cam)->where('dateparution','<=', now())->orderByDesc('dateparution')
                ->leftJoin('pays','pays.code','=','articles.pays_code')
                ->leftJoin('rubriques','rubriques.id','=','articles.rubrique_id')
-               ->select('*')->take(5);
+                ->leftJoin('categories','categories.id','=','rubriques.categorie_id')
+               ->select('*')->take(100);
        });
         return Cache::get($cam)->take(100);
-        
     }
-    public static  function getOtherNews($orther) {
+    public  function getOtherNews($orther) {
         if(Cache::has($orther)){
             return Cache::get($orther)->take(100);
         }
-        Cache::remember($orther, now()->addHour(1),function(){
+        Cache::remember($orther, now()->addHour(1),function() use ($orther) {
             return Article::where('articles.pays_code','<>',$orther)->where('dateparution','<=', now())->orderByDesc('dateparution')
                ->leftJoin('pays','pays.code','=','articles.pays_code')
                ->leftJoin('rubriques','rubriques.id','=','articles.rubrique_id')
-               ->select('*')->take(5);
+                ->leftJoin('categories','categories.id','=','rubriques.categorie_id')
+               ->select('*')->take(100);
        });
-        return Cache::get($cam)->take(100);
+        return Cache::get($orther)->take(100);
     }
-    public function findAll(){
+    public  function getNewsBySlug($slug) {
+
+        $article= Article::where('slug',$slug)->increment('hit');
+        return Article::where('slug',$slug)
+            ->leftJoin('pays','pays.code','=','articles.pays_code')
+            ->leftJoin('rubriques','rubriques.id','=','articles.rubrique_id')
+            ->leftJoin('categories','categories.id','=','rubriques.categorie_id')
+            ->select('*')->get();
+    }
+    public function getNewsSameRubrique($rubrique)
+    {
+
+        if(Cache::has($rubrique)){
+            return Cache::get($rubrique)->take(5);
+        }
+        Cache::remember($rubrique, now()->addHour(1),function() use ($rubrique) {
+            return Article::where('articles.rubrique_id',$rubrique)->where('dateparution','<=', now())->orderByDesc('dateparution')
+                ->leftJoin('pays','pays.code','=','articles.pays_code')
+                ->leftJoin('categories','categories.id','=','rubriques.categorie_id')
+                ->leftJoin('rubriques','rubriques.id','=','articles.rubrique_id')
+                ->select('*')->take(5)->get();
+        });
+        return Cache::get($rubrique)->take(5);
+
+    }
+    /*public function findAll(){
         if($article=Cache::get('article-list')){
             return $article;
         }
         $article=Article::orderBy('dateparution','desc')->paginate(100);
         Cache::set('article-list',$article,Carbon::now()->addMinute(60));
         return $article;
-    }
+    }*/
 
 }
